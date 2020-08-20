5dd85c9cb6bd87b8f50239530169ff8728efbb6e
sebaek <sebaek@gmail.com>
2020-08-20 15:50:27 +0900

    mybatis

--- src/main/java/org/zerock/config/RootConfig.java
+++ src/main/java/org/zerock/config/RootConfig.java
@@ -1,10 +1,50 @@
 package org.zerock.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
 @Configuration
 @ComponentScan(basePackages = "org.zerock.sample")
 public class RootConfig {
-
+	@Bean 
+	public HikariConfig hikariConfig() {
+		HikariConfig hikariConfig = new HikariConfig();
+		hikariConfig.setDriverClassName("oracle.jdbc.driver.OracleDriver");
+		hikariConfig.setJdbcUrl("jdbc:oracle:thin:@localhost:1521:XE");
+		hikariConfig.setUsername("book_ex");
+		hikariConfig.setPassword("book_ex");
+		return hikariConfig;
+	}
+	
+	@Bean(destroyMethod = "close")
+	public DataSource dataSource(HikariConfig hikariConfig) {
+		HikariDataSource dataSource = new HikariDataSource(hikariConfig);
+		return dataSource;
+	}
+	
+	@Bean
+	public SqlSessionFactoryBean sqlSessionFactory(DataSource dataSource) {
+		SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
+		bean.setDataSource(dataSource);
+		
+		return bean;
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
